e304bc9659d41789a1bb6db660932afe34111f78
Chibuzo Nwakama <chibuzo-nwakama@uiowa.edu>
2017-11-14 20:05:09 -0600

    new changes

--- fraction/fraction.py
+++ fraction/fraction.py
@@ -34,7 +34,7 @@ class Fraction(object):
             return str(self.num) + "/" + str(self.den)        
 
     def __float__(self):
-        return self.num/self.den
+        return float(self.num)/float(self.den)
 
     @property
     def eval(self):
